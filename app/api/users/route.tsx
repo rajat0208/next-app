4d2b23a4f2ae47949f095cf0e4bd3e69de7b367a
rajat0208 <rajatshrestha0208@gmail.com>
2024-12-23 22:03:20 +0545

    POST (creatiing an object)

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -8,3 +8,11 @@ export function GET(request:NextRequest){
         ]
     )
 }
+
+export async function POST(request:NextRequest){
+   const body= await request.json();
+   if(!body.name){
+    return NextResponse.json({error:"Name is required"},{status:400})
+   }
+    return NextResponse.json({id:1,name:body.name},{status:201});
+}
