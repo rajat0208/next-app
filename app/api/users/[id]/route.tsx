b247709fe7b4c32e4ba55862ff29d7370eb4eb82
rajat0208 <rajatshrestha0208@gmail.com>
2024-12-24 11:49:07 +0545

    DELETE request

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -7,3 +7,22 @@ export function GET(request:NextRequest,{params}:{params:{id:number}}){
     }
     return NextResponse.json({id:1, name:"Rajat"})
 }
+
+export async function PUT(request:NextRequest ,{params}:{params:{id: number}}){
+    const body= await request.json();
+    if(!body.name){
+     return NextResponse.json({error:"Name is required"},{status:400})
+    }
+    if(params.id>10){
+        return NextResponse.json({error:"user not found"},{status:400})
+    }
+     return NextResponse.json({id:1,name:body.name},{status:201});
+ }
+
+ 
+export function DELETE(request:NextRequest, {params}:{params:{id: number}}) {
+    if(params.id>10)
+        return NextResponse.json({error:"User Not Foud"},{status:400})
+  
+    return NextResponse.json({})
+}
