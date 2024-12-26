6ef597094d5bb418f2473b684f448095a37b778b
rajat0208 <rajatshrestha0208@gmail.com>
2024-12-26 22:36:12 +0545

    getting data

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,12 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../schema";
+import prisma from "@/prisma/client";
 
-export function GET(request:NextRequest,{params}:{params:{id:number}}){
+export async function GET(request:NextRequest,{params}:{params:{id:string}}){
 
-    if(params.id>10){
+    const user= await prisma.user.findUnique({
+        where:{id:parseInt(params.id)}
+    })
+
+    if(!user){
         return NextResponse.json({error:"User Not Found", status:404})
     }
-    return NextResponse.json({id:1, name:"Rajat"})
+    return NextResponse.json(user)
 }
 
 export async function PUT(request:NextRequest ,{params}:{params:{id: number}}){
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,13 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client";
 
-export function GET(request:NextRequest){
-    return NextResponse.json(
-        [
-            {id:1, name:"Rajat"},
-            {id:2, name:"Shrestha"}
-        ]
-    )
+export async function GET(request:NextRequest){
+    const users= await prisma.user.findMany();
+    return NextResponse.json(users)
 }
 
 export async function POST(request:NextRequest){
