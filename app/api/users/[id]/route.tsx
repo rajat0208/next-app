9520e0783e66547c1b0f425e791f8d6be92aad55
rajat0208 <rajatshrestha0208@gmail.com>
2024-12-26 23:20:44 +0545

    updating data

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -14,16 +14,31 @@ export async function GET(request:NextRequest,{params}:{params:{id:string}}){
     return NextResponse.json(user)
 }
 
-export async function PUT(request:NextRequest ,{params}:{params:{id: number}}){
+export async function PUT(request:NextRequest ,{params}:{params:{id:string}}){
     const body= await request.json();
     const validation=schema.safeParse(body)
     if(!validation.success){
      return NextResponse.json(validation.error.errors,{status:400})
     }
-    if(params.id>10){
+    
+    const user= await prisma.user.findUnique({
+        where:{id:parseInt(params.id)}
+    })
+    
+    if(!user){
         return NextResponse.json({error:"user not found"},{status:400})
     }
-     return NextResponse.json({id:1,name:body.name},{status:201});
+
+    const updatedUser = await prisma.user.update({
+        where:{id:user.id},
+        data :{
+            name:body.name,
+            email:body.email
+        }
+    }
+       
+    )
+     return NextResponse.json(updatedUser,{status:201});
  }
 
  
