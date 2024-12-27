b683d7bc3de9988444816fce92d0621dfe9f73bc
rajat0208 <rajatshrestha0208@gmail.com>
2024-12-27 23:21:33 +0545

    customizing upload widgets

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -12,6 +12,27 @@ const uploadPage = () => {
     <>
     {publicId && <CldImage src={publicId} width={270} height={180} alt='A anime image'/>}
         <CldUploadWidget uploadPreset='next-app'
+        options={{
+            sources:["local"],
+            multiple:false,
+            maxFiles:5,
+            styles:{
+                palette: {
+                    window: "#FFFFFF",
+                    windowBorder: "#90A0B3",
+                    tabIcon: "#0078FF",
+                    menuIcons: "#455871",
+                    textDark: "#000000",
+                    textLight: "#FFFFFF",
+                    link: "#0078FF",
+                    action: "#FF620C",
+                    inactiveTabIcon: "#0E2F5A",
+                    error: "#F44235",
+                    inProgress: "#0078FF",
+                    complete: "#20B832",
+                    sourceBg: "#E4EBF1"
+            }
+        }}}
         onUploadAdded={(result, widget)=>{
             if(result.event !== "success") return;
             const info=result.info as CloudinaryResult;
