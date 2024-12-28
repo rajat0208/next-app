67492fce6d41b017bcd8a9e3846234e03d4e55ac
rajat0208 <rajatshrestha0208@gmail.com>
2024-12-28 23:15:31 +0545

    configure goggle provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,13 @@
 import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google";
 
-const handler= NextAuth({})
+const handler= NextAuth({
+    providers:[
+        GoogleProvider({
+            clientId: process.env.GOGGLE_CLIENT_ID !,
+            clientSecret: process.env.GOGLE_GOGGLE_CLIENT_SECRET !
+        })
+    ]
+})
 
 export {handler as GET, handler as POST}
--- app/navBar.tsx
+++ app/navBar.tsx
@@ -3,9 +3,10 @@ import React from 'react'
 
 const navBar = () => {
   return (
-    <div className='flex bg-slate-700 p-4 text-cyan-50' >
+    <div className='flex bg-slate-700 p-4 text-cyan-50 space-x-3' >
         <Link href="/" className='mr-5'>Next.Js</Link>
         <Link href="/users">Users</Link>
+        <Link href="/api/auth/signin">Login</Link>
     </div>
   )
 }
