a608a0f0e5658ee2af3dc38e2acb5624965fb4b1
rajat0208 <rajatshrestha0208@gmail.com>
2024-12-29 22:14:26 +0545

    accessing session on the server

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 
-const handler= NextAuth({
+export const authOptions=({
     providers:[
         GoogleProvider({
             clientId: process.env.GOGGLE_CLIENT_ID !,
@@ -9,5 +9,6 @@ const handler= NextAuth({
         })
     ]
 })
+const handler= NextAuth(authOptions)
 
 export {handler as GET, handler as POST}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import ProductCard from './components/productCard'
-export default function Home() {
+import { getServerSession } from 'next-auth'
+import { authOptions } from "./api/auth/[...nextauth]/route"
+export default  async function Home() {
+
+  const session= await getServerSession(authOptions);
   return (
     <main>
-      <h1>HHHHHHHHHHHEEEEEEEEEELLLLLLLOOOOOOOOO</h1>
+      <h1>HELLO {session && <span>{session.user!.name}</span>}</h1>
       <Link href="/users">Users</Link>
       <ProductCard />
     </main>
