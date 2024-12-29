ce0f58f7d95d266cbc9fb71f145721c5c683b378
rajat0208 <rajatshrestha0208@gmail.com>
2024-12-29 21:37:24 +0545

    fixed goggle signin issue

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,7 @@ const handler= NextAuth({
     providers:[
         GoogleProvider({
             clientId: process.env.GOGGLE_CLIENT_ID !,
-            clientSecret: process.env.GOGLE_GOGGLE_CLIENT_SECRET !
+            clientSecret: process.env.GOGGLE_CLIENT_SECRET !
         })
     ]
 })
